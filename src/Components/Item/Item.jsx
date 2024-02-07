1f991f7783debb09c3bec481dcf618a96dc1676e
Lisandro Rodriguez <licharp41@gmail.com>
2024-02-07 19:34:14 -0300

    actualizacion json

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,6 +2,7 @@ const Item = ({ producto }) => {
   return (
     <>
       <div key={producto.id}>
+        <img src={producto.imagen} alt={producto.nombre} />
         <h2># {producto.id}</h2>
         <h3>Nombre: {producto.nombre}</h3>
         <h3>Precio: ${producto.precio}</h3>
