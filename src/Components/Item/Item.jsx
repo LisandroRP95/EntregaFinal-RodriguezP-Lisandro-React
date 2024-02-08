33b4a4aee7ee46adda2f4150f6dc16f64db7ab5a
Lisandro Rodriguez <licharp41@gmail.com>
2024-02-07 21:01:00 -0300

    solucion ruta imagenes json

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,7 +6,7 @@ const Item = ({ producto }) => {
         <h2># {producto.id}</h2>
         <h3>Nombre: {producto.nombre}</h3>
         <h3>Precio: ${producto.precio}</h3>
-        <h3>Categoria:{producto.categoria}</h3>
+        <h3>Categoria: {producto.categoria}</h3>
       </div>
     </>
   );
